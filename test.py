8bb1780ff577ffbdb65bcf879ea1eca0e79fd330
Vinicíus Araújo <viniamaral200621@gmail.com>
2026-07-13 14:16:49 -0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import pytesseract
-
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-img = cv2.imread('image.png')
-texto = pytesseract.image_to_string(img)
-print(texto)
